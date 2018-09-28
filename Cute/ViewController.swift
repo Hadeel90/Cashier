94c354685cb7d767ea097bf003cd6a37491a8e58
Hadeel Alharthi <hadeel.alharthi@hotmail.com>
2018-09-28 21:13:26 +0300

    Initial Commit

---
+++ Cute/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Cute
+//
+//  Created by Hadeel on 9/28/18.
+//  Copyright © 2018 Hadeel Alharthi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
