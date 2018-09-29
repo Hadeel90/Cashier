28e61e8e8862ba1d90d801c66ca31b1613f4a555
Hadeel Alharthi <hadeel.alharthi@hotmail.com>
2018-09-29 22:01:03 +0300

    Add windows and view controllers in design mode

--- Cute/ViewController.swift
+++ Cute/ViewController.swift
@@ -9,10 +9,13 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
+    //outlets and variables
+    @IBOutlet weak var AdminUsernameField: NSTextField!
+    @IBOutlet weak var AdminPasswordField: NSSecureTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
@@ -21,7 +24,11 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
+    
+    //actions and methods
+    @IBAction func NewReceiptButton(_ sender: NSButton) {
+        performSegue(withIdentifier: "NewReceiptSegue", sender: self)
+    }
 
 }
 
