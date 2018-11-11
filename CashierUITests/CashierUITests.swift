ea860888fdc218141873180836bcc3c88288d608
Hadeel Alharthi <hadeel.alharthi@hotmail.com>
2018-11-11 16:33:27 +0300

    rename project

--- CashierUITests/CashierUITests.swift
+++ CashierUITests/CashierUITests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class CuteUITests: XCTestCase {
+class CashierUITests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
